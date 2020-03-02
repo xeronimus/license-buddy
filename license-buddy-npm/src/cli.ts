ef701fe87b7614867ce2a1a2af2a255e9faac14c
xeronimus@gmail.com <sTrentwow890>
2020-03-02 20:19:26 +0100

    adds bin/license-buddy.ts  that invokes cli.ts with process.argv.slice(2)

--- license-buddy-npm/src/cli.ts
+++ license-buddy-npm/src/cli.ts
@@ -1,78 +1,79 @@
-#!/usr/bin/env node
-
 import LicenseBuddy from './LicenseBuddy';
 import yargs from 'yargs';
 
-yargs
-    .scriptName('license-buddy-npm')
-    .usage('Usage: $0 <cmd> [args]')
-    .command(
-        'analyze [rootPath]',
-        'Analyze licenses in your npm dependency tree',
-        (yargs) => {
-            yargs
-                .positional('rootPath', {
-                    type: 'string',
-                    describe: 'the path to the root directory, where your "package.json" lies'
-                })
-                .demand('rootPath')
-                .option('v', {
-                    alias: 'verbose',
-                    default: false,
-                    describe: 'Print out more detailed information',
-                    type: 'boolean'
-                })
-                .option('p', {
-                    alias: 'production',
-                    default: false,
-                    describe: 'Only include production dependencies',
-                    type: 'boolean'
-                })
-                .option('d', {
-                    alias: 'development',
-                    default: false,
-                    describe: 'Only include development dependencies',
-                    type: 'boolean'
-                });
-        },
-        analyzeHandler
-    )
+export default function cli(args: string[]) {
+    yargs
+        .scriptName('license-buddy-npm')
+        .usage('Usage: $0 <cmd> [args]')
+        .command(
+            'analyze [rootPath]',
+            'Analyze licenses in your npm dependency tree',
+            (yargs) => {
+                yargs
+                    .positional('rootPath', {
+                        type: 'string',
+                        describe: 'the path to the root directory, where your "package.json" lies'
+                    })
+                    .demand('rootPath')
+                    .option('v', {
+                        alias: 'verbose',
+                        default: false,
+                        describe: 'Print out more detailed information',
+                        type: 'boolean'
+                    })
+                    .option('p', {
+                        alias: 'production',
+                        default: false,
+                        describe: 'Only include production dependencies',
+                        type: 'boolean'
+                    })
+                    .option('d', {
+                        alias: 'development',
+                        default: false,
+                        describe: 'Only include development dependencies',
+                        type: 'boolean'
+                    });
+            },
+            analyzeHandler
+        )
 
-    .command(
-        'check [rootPath]',
-        'Check licenses in your npm dependency tree against recommendations. Warn if problematic license is used.',
-        (yargs) => {
-            yargs
-                .positional('rootPath', {
-                    type: 'string',
-                    describe: 'the path to the root directory, where your "package.json" lies'
-                })
-                .demand('rootPath')
-                .option('p', {
-                    alias: 'production',
-                    default: false,
-                    describe: 'Only include production dependencies',
-                    type: 'boolean'
-                })
-                .option('d', {
-                    alias: 'development',
-                    default: false,
-                    describe: 'Only include development dependencies',
-                    type: 'boolean'
-                })
-                .option('c', {
-                    alias: 'ci',
-                    default: false,
-                    describe: 'CI mode: fail on violations',
-                    type: 'boolean'
-                });
-            // TODO:  discuss whether we should add option to specify rules file
-        },
-        checkHandler
-    )
+        .command(
+            'check [rootPath]',
+            'Check licenses in your npm dependency tree against recommendations. Warn if problematic license is used.',
+            (yargs) => {
+                yargs
+                    .positional('rootPath', {
+                        type: 'string',
+                        describe: 'the path to the root directory, where your "package.json" lies'
+                    })
+                    .demand('rootPath')
+                    .option('p', {
+                        alias: 'production',
+                        default: false,
+                        describe: 'Only include production dependencies',
+                        type: 'boolean'
+                    })
+                    .option('d', {
+                        alias: 'development',
+                        default: false,
+                        describe: 'Only include development dependencies',
+                        type: 'boolean'
+                    })
+                    .option('c', {
+                        alias: 'ci',
+                        default: false,
+                        describe: 'CI mode: fail on violations',
+                        type: 'boolean'
+                    });
+                // TODO:  discuss whether we should add option to specify rules file
+            },
+            checkHandler
+        )
 
-    .demandCommand(1, 'must provide a valid command')
-    .help().argv;
+        .demandCommand(1, 'must provide a valid command')
+        .help()
+        .parse(args);
+}
 
 async function analyzeHandler(argv: any) {
     const lb = new LicenseBuddy(argv.rootPath);
